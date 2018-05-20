0fe09a4ba7e503f13333dab151faa5ea779a2c3c
Guillermo Lopez <guillermolopez2529@gmail.com>
2018-05-20 13:20:08 -0400

    Refactor

--- index.js
+++ index.js
@@ -1,33 +1,6 @@
 'use strict'
 
-const getServices = require('./lib/getServices')
+const Opery = require('opery')
 
-async function init (options) {
-  const db = { services: {} }
-  const { adapter } = options
-  const orm = await adapter.setupOrm(options.orm)
-  const models = await adapter.getModels(orm, options.modelsDir)
-  const services = await getServices(options.servicesDir)
-  const baseService = options.adapter.baseService
+module.exports = new Opery()
 
-  Object.keys(models).forEach(modelName => {
-    db.services[modelName] = baseService(models[modelName])
-  })
-  Object.keys(models).forEach(modelName => {
-    let service = services[modelName]
-
-    if (!service) {
-      return
-    }
-
-    service = service(models[modelName])
-    db.services[modelName] = Object.assign({}, db.services[modelName], service)
-  
-  })
-
-  return db
-}
-
-module.exports = {
-  init
-}
